57b0beb87e466fd1efdbe39497ee1e705b60e926
Huang-Ming Chang <huang.m.chang@gmail.com>
2019-09-25 10:07:13 +0200

    Updates

---
+++ precache-manifest.8ec8005d36d6da35ab7501260d107654.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec66fde038a0fe6bdebffa6f87c95941",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59185cc39769c6ac790d",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "1c831bbdf55dca4bc01c",
+    "url": "/static/js/2.2b4c107a.chunk.js"
+  },
+  {
+    "revision": "59185cc39769c6ac790d",
+    "url": "/static/js/main.3b7a40d0.chunk.js"
+  },
+  {
+    "revision": "ce695e713b6b27819ce0",
+    "url": "/static/js/runtime-main.d9dd6c06.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
