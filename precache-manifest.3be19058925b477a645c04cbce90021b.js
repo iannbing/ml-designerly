70d3a088e146983a6c6c2e1749bed5f253f90070
Huang-Ming Chang <huang.m.chang@gmail.com>
2019-09-25 10:51:39 +0200

    Updates

---
+++ precache-manifest.3be19058925b477a645c04cbce90021b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bcc506c431badaab09841913ba9cc60",
+    "url": "/ml-designerly/index.html"
+  },
+  {
+    "revision": "039bc51bcd5a97f293e2",
+    "url": "/ml-designerly/static/js/2.88edae0c.chunk.js"
+  },
+  {
+    "revision": "e47892b097845584803d",
+    "url": "/ml-designerly/static/js/main.a0b57fbd.chunk.js"
+  },
+  {
+    "revision": "c9e61768b5011837b538",
+    "url": "/ml-designerly/static/js/runtime-main.f3a7e7a6.js"
+  }
+]);
